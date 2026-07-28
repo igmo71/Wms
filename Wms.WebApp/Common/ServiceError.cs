26343c9cf4b65637f7586318ac030089b16b592a
Igor Mogilnitskiy <igmo71@outlook.com>
2026-07-28 09:49:15 +0300

    Refactor ServiceError.

--- Wms.WebApp/Common/ServiceError.cs
+++ Wms.WebApp/Common/ServiceError.cs
@@ -1,3 +1,8 @@
 ﻿namespace Wms.WebApp.Common;
 
-public record ServiceError(ServiceErrorType Type, string? Message);
+public record ServiceError(ServiceErrorType Type, string? Message)
+{
+    public static ServiceError Invalid<TEntity>(string? message = null) => new(ServiceErrorType.Invalid, message ?? $"{typeof(TEntity).Name} is invalid.");
+    public static ServiceError NotFound<TEntity>(string? message = null) => new(ServiceErrorType.NotFound, message ?? $"{typeof(TEntity).Name} not found.");
+    public static ServiceError Failure<TEntity>(string? message = null) => new(ServiceErrorType.Failure, message ?? $"{typeof(TEntity).Name} failed.");
+}
