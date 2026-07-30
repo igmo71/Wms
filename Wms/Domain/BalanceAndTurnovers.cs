9d8ecec7d386feba485234748d15a7628f9bf3f7
Igor Mogilnitskiy <igmo71@outlook.com>
2026-07-30 09:07:19 +0300

    BalanceAndTurnovers

--- Wms/Domain/BalanceAndTurnovers.cs
+++ Wms/Domain/BalanceAndTurnovers.cs
@@ -4,6 +4,8 @@ public class BalanceAndTurnovers
 {
     public Guid Id { get; set; }
 
+    public DateTime DateTime { get; set; }
+
     public Guid StockKeepingUnitId { get; private set; }
     public StockKeepingUnit? StockKeepingUnit { get; private set; }
 
@@ -13,12 +15,11 @@ public class BalanceAndTurnovers
     public Guid StorageLocationId { get; private set; }
     public StorageLocation? StorageLocation { get; private set; }
 
-    public Guid? TransactionId { get; set; }
-    public BalanceTransaction? Transaction { get; set; }
+    public Guid? RecorderId { get; set; }
+    public string? RecorderType { get; set; }
+    public int LineNumber { get; set; }
 
-    public decimal OpeningBalance { get; set; }
-    public decimal Turnover { get; set; }
-    public decimal Receipt { get; set; }
-    public decimal Expense { get; set; }
-    public decimal ClosingBalance { get; set; }
+    public decimal QuantityDelta { get; private set; }
+    public decimal BalanceBefore { get; private set; }
+    public decimal BalanceAfter { get; private set; }
 }
--- Wms/Domain/BalanceTransactionType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Wms.Domain;
-
-internal enum BalanceTransactionType
-{
-    Correction = 1,
-    Receiving = 2,
-    Shipment = 3,
-    Transfer = 4
-}
