ea5bace42e8524390ab50ccd2d9cba298fa118b6
Igor Mogilnitskiy <igmo71@outlook.com>
2026-08-08 12:30:12 +0300

    Document_РасходныйОрдерНаТовары_ТоварыПоРаспоряжениям

--- Wms/Integration/OneS/Models/Document_РасходныйОрдерНаТовары_ТоварыПоРаспоряжениям.cs
+++ Wms/Integration/OneS/Models/Document_РасходныйОрдерНаТовары_ТоварыПоРаспоряжениям.cs
@@ -2,7 +2,7 @@
 
 namespace Wms.Integration.OneS.Models;
 
-internal class Document_РасходныйОрдерНаТовары_ОтгружаемыеТовары
+internal class Document_РасходныйОрдерНаТовары_ТоварыПоРаспоряжениям
 {
     public Guid Ref_Key { get; set; }
 
@@ -11,8 +11,8 @@ internal class Document_РасходныйОрдерНаТовары_Отгру�
 
     public Guid Номенклатура_Key { get; set; }
     public double Количество { get; set; }
-    public double КоличествоУпаковок { get; set; }
-    public string? Действие { get; set; }
+    public Guid? Распоряжение { get; set; }
+    public string? Распоряжение_Type { get; set; }
 
     //public Guid? Характеристика_Key { get; set; }
 }
