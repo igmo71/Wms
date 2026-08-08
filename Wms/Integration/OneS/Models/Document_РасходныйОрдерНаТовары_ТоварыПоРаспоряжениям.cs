ef1f49c7151383d5b75bc88aca845854f96b688a
Igor Mogilnitskiy <igmo71@outlook.com>
2026-08-08 12:37:52 +0300

    www

---
+++ Wms/Integration/OneS/Models/Document_РасходныйОрдерНаТовары_ТоварыПоРаспоряжениям.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Wms.Integration.OneS.Models;
+
+internal class Document_РасходныйОрдерНаТовары_ТоварыПоРаспоряжениям
+{
+    public Guid Ref_Key { get; set; }
+
+    [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
+    public int LineNumber { get; set; }
+
+    public Guid Номенклатура_Key { get; set; }
+    public double Количество { get; set; }
+    public Guid? Распоряжение { get; set; }
+    public string? Распоряжение_Type { get; set; }
+
+    //public Guid? Характеристика_Key { get; set; }
+}
